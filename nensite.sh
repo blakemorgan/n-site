72d373b70f2df9c1df416d68243c80fc6d87d3b6
Blake Morgan <b.morgan.296@gmail.com>
2018-05-17 09:19:38 -0600

    Added scripts

---
+++ nensite.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ ! $1 ]; then
+    echo "No site configuration"
+    exit;
+fi
+
+if [ -f /etc/nginx/sites-available/$1 ]; then
+    if [ $(ln -s /etc/nginx/sites-available/$1 /etc/nginx/sites-enabled/$1) ]; then
+        exit;
+    fi
+else
+    echo "$1 does not exist as a site configuraiton."
+fi
